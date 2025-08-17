afa157733ebe6272c9e3de9063f8171de493b449
Eleazar Lima <edrxdrdr@gmail.com>
2025-08-17 18:02:48 -0300

    Create TopicoController.java

---
+++ controller/TopicoController.java
@@ -0,0 +1,14 @@
+package com.forumhub.controller;
+
+import com.forumhub.domain.topico.*;
+import com.forumhub.domain.curso.CursoRepository;
+import com.forumhub.domain.usuario.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Reque
